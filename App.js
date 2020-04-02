06bf6abac5c9ffb43416700b582459268ba6ee68
jongjetW <jongjet.w@gmail.com>
2020-04-02 18:23:17 +1100

    guess game 1.0

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
 
 // export default function App() {
 //   return (
@@ -10,11 +10,73 @@ import { StyleSheet, Text, View } from 'react-native';
 // }
 
 export default class Game extends Component {
+//set initial state
+state = { // state is the obj
+  secret: 0,
+  input: '',
+  feedback: ''
+}
+
+//function to gen random no
+genRandom(){
+  return Math.round(Math.random() * 100) //we can use only bracket too
+}
+
+//function to initialize the game
+init(){
+  const secretNo = this.genRandom()
+  this.setState({secret: secretNo})
+}
+
+//lifecycle fn
+componentDidMount(){
+  this.init()
+
+}
+
+//update input state
+updateInput = (value) => {
+  this.setState({input: value})
+}
+
+//check no fn
+chkGuess = () => {
+  const userGuess = parseInt(this.state.input)
+  const secretNo = this.state.secret
+  if (userGuess == secretNo){
+    //feedback
+    this.setState({feedback: 'Correct! the ans is ' + secretNo})
+  }
+  if (userGuess < secretNo){
+    this.setState({feedback: "It's smaller than " + userGuess})
+  }
+  if (userGuess > secretNo){
+    this.setState({feedback: "It's larger than " + userGuess + ' ' +secretNo})
+  }
+  return
+}
+
   render () {
     return(
       <View style={styles.container}>
-        <Text>
-          Guess my number....
+        <Text style={styles.text}>
+          Guess the number....
+        </Text>
+
+        <TextInput 
+        style={styles.input} 
+        keyboardType='number-pad'
+        onChangeText={this.updateInput}>
+        </TextInput>
+
+        <TouchableHighlight 
+        style={styles.btn}
+        underlayColor='white'
+        onPress={this.chkGuess}>
+          <Text>Submit</Text>
+        </TouchableHighlight>
+        <Text style={styles.text}>
+          {this.state.feedback}
         </Text>
       </View>
     )
@@ -23,14 +85,30 @@ export default class Game extends Component {
 }
 
 const styles = StyleSheet.create({
+
+  btn:{
+    width: 100,
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: 'white',
+    alignItems: 'center'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   text:{
-    color: '#ff33ff',
+    color: 'white',
+    fontSize: 30
+  },
+  input:{
+    backgroundColor: 'gray',
+    width: 100,
     fontSize: 30,
+    textAlign: 'center',
+    marginTop: 20,
+    padding: 10
   }
 });
